20aad668194e7b905f2edc145e4507d1f39aba01
Eggboy-cloud <egil@guting.se>
2023-01-31 14:54:13 +0100

    hjälp

--- lab_1/src/Lab1.java
+++ lab_1/src/Lab1.java
@@ -15,12 +15,14 @@ public class Lab1 {
     down_left.add(5,9);
     down_left.add(4,11);
     down_left.add(5,10);
+    down_left.add(1,10);
     Critical down_middle = new Critical(1);
     Critical down_right = new Critical(2);
     down_right.add(14,9);
     down_right.add(16,7);
     down_right.add(14,10);
     down_right.add(16,8);
+    doqn_left.add(19,8);
     Critical down = new Critical(3);
     down.add(10,11);
     down.add(10,13);
@@ -137,9 +139,10 @@ public void setSwitch(int x, int y, int dir) {
     private final int speed;
     private SensorEvent sensor;
     private Critical current;
+    private Critical rail;
     //swtiches 15 9 right to zero cluster
     // switches 17 7 last before 4-crossing
-    private boolean isAcquired = false;
+    private boolean dir = false;
 
     public Train(int id, int speed){
       this.id = id;
@@ -160,21 +163,19 @@ public void setSwitch(int x, int y, int dir) {
     int getIndex(Critical c) {
       for (Lab1.Critical.Sensor sensor : c.positions) {
         if(sensor.x == this.sensor.getXpos() && sensor.y == this.sensor.getYpos())
-          return c.indexOf(sensor);
-          break;
+            return c.indexOf(sensor);
       }
       return -1;
     }
 
     void check_track(Critical c, Train t){
-      
       int index = getIndex(c);
       //TODO make exception
       
       t.setSpeed(0);
       switch(c.id) {
         case(0): t.changeSection(c); section_zero(t,index); break; 
-        case(2): t.changeSection(c); setSwitch(17, 7,tsi.SWITCH_RIGHT);//section_two(t,index); break;
+        case(2): t.changeSection(c); section_two(t,index);  break;
         case(3): t.changeSection(c); //TODO reverse train
         break;
         case(5): t.changeSection(c); //TODO cross-section;
@@ -188,7 +189,10 @@ public void setSwitch(int x, int y, int dir) {
 
     public void changeSection(Critical c) {
       try {
-        c.acquire(); this.current.release(); this.current = c;
+        c.acquire(); 
+        if(this.current !=null)
+          this.current.release(); 
+        this.current = c;
       }
       catch (InterruptedException e) {
         e.printStackTrace();
@@ -202,28 +206,19 @@ public void setSwitch(int x, int y, int dir) {
       int switch2_x = 4;
       int switch2_y = 9;
 
-      if(!t.isAcquired && (sensor == 0 || sensor == 2)) {
-            if(sections.get(1).tryAcquire())
-              setSwitch(switch2_x, switch2_y,tsi.SWITCH_LEFT);
-            else 
-              setSwitch(switch2_x, switch2_y, tsi.SWITCH_RIGHT);
-            t.isAcquired = true;
-          }
-      else if(!t.isAcquired && (sensor == 1 || sensor == 3)) {
-            if(sections.get(3).tryAcquire())
-              setSwitch(switch1_x, switch1_y, tsi.SWITCH_LEFT);
-            else
-              setSwitch(switch1_x, switch1_y,tsi.SWITCH_RIGHT);
-            t.isAcquired = true;
-          }
-      else {
-            t.isAcquired = false;
-          }
+      
       switch(sensor){
-        case(0): setSwitch(switch1_x, switch1_y,tsi.SWITCH_LEFT);   break;
-        case(2): setSwitch(switch1_x, switch1_y, tsi.SWITCH_RIGHT); break;
-        case(1): setSwitch(switch2_x, switch2_y,tsi.SWITCH_LEFT);   break;
-        case(3): setSwitch(switch2_x, switch2_y, tsi.SWITCH_RIGHT); break;
+        case(0): setSwitch(switch1_x, switch1_y,tsi.SWITCH_RIGHT); dir=true;  break;
+        case(2): setSwitch(switch1_x, switch1_y, tsi.SWITCH_LEFT); dir=true;  break;
+        case(1): setSwitch(switch2_x, switch2_y,tsi.SWITCH_LEFT);  dir=false; break;
+        case(3): setSwitch(switch2_x, switch2_y, tsi.SWITCH_RIGHT);dir=false; break;
+        case(4): 
+          if(dir) {
+            if(sections.get(1).tryAcquire()){
+              this.current = sections.get(1);
+
+            }
+          }
       }
     }
 
@@ -233,28 +228,31 @@ public void setSwitch(int x, int y, int dir) {
       int switch3_y = 9;
       int switch4_x = 17;
       int switch4_y = 7;
-      if(!t.isAcquired && (sensor == 1 || sensor == 3)) {
-        if(sections.get(1).tryAcquire())
-              setSwitch(switch3_x, switch3_y, tsi.SWITCH_RIGHT);
-        else
-              setSwitch(switch3_x, switch3_y,tsi.SWITCH_LEFT);
-        t.isAcquired = true;
-      }           
-      else if(!t.isAcquired && (sensor == 2 || sensor == 0)) {
+      if(t.isAcquired && (sensor == 1 || sensor == 3)) {
         if(sections.get(4).tryAcquire())
               setSwitch(switch4_x, switch4_y,tsi.SWITCH_RIGHT);
         else 
               setSwitch(switch4_x, switch4_y, tsi.SWITCH_LEFT);
-        t.isAcquired = true;
-      }
-      else {
+        t.isAcquired = false;
+      }           
+      else if(t.isAcquired && (sensor == 2 || sensor == 0)) {
+        if(sections.get(1).tryAcquire())
+              setSwitch(switch3_x, switch3_y, tsi.SWITCH_RIGHT);
+        else
+              setSwitch(switch3_x, switch3_y,tsi.SWITCH_LEFT);
         t.isAcquired = false;
       }
+      else {
+        System.out.println("Acquring...");
+        t.isAcquired = true;
+      }
+      System.out.println(sensor);
       switch(sensor){
         case(0): setSwitch(switch3_x, switch3_y, tsi.SWITCH_RIGHT); break;
-        case(2): setSwitch(switch3_x, switch3_y, tsi.SWITCH_LEFT);  break;
         case(1): setSwitch(switch4_x, switch4_y, tsi.SWITCH_RIGHT);
-        System.out.println("On sensor"); break;
+          System.out.println("On sensor"); 
+          break;
+        case(2): setSwitch(switch3_x, switch3_y, tsi.SWITCH_LEFT);  break;
         case(3): setSwitch(switch4_x, switch4_y, tsi.SWITCH_LEFT);  break;
       }
     }
@@ -262,6 +260,7 @@ public void setSwitch(int x, int y, int dir) {
       while(true){
         try {
           this.sensor = tsi.getSensor(this.id);
+          System.out.println(this.sensor.getXpos() + " " + this.sensor.getYpos());
           if(this.sensor.getStatus()==1) {
             Critical current = isCritical(sensor.getXpos(), sensor.getYpos());
             System.out.println("sensor from " + current.id + " triggered");
