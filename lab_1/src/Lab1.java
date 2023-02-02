41390c010f63c238a41795823c35edece73d7ca3
Eggboy-cloud <egil@guting.se>
2023-02-02 13:41:34 +0100

    högre speeds

--- lab_1/src/Lab1.java
+++ lab_1/src/Lab1.java
@@ -3,26 +3,33 @@ import java.util.ArrayList;
 import java.util.concurrent.*;
 
 public class Lab1 {
-  ArrayList<Critical> sections = new ArrayList(); 
+  ArrayList<Critical> sections = new ArrayList<Critical>(); 
   TSimInterface tsi = TSimInterface.getInstance();
+  int SWITCH_LEFT = TSimInterface.SWITCH_LEFT;
+  int SWITCH_RIGHT = TSimInterface.SWITCH_RIGHT;
   
 
-
+  /**
+   * Instantiates objects from the Class Critical which then adds sensors to each section
+   * Adds all instances of Critical to the ArrayList sections
+   * @param speed1
+   *              speed of the first train
+   * @param speed2
+   *              speed of the second train
+   */
   public Lab1(int speed1, int speed2) {
-    tsi.setDebug(true);
+    
     Critical down_left = new Critical(0);
     down_left.add(3,13);
     down_left.add(7,9);
     down_left.add(6,11);
     down_left.add(6,10);
-    //down_left.add(1,10);
     Critical down_middle = new Critical(1);
     Critical down_right = new Critical(2);
     down_right.add(12,9);
     down_right.add(14,7);
     down_right.add(13,10);
     down_right.add(15,8);
-    //down_right.add(19,8);
     Critical down = new Critical(3);
     down.add(10,11);
     down.add(10,13);
@@ -32,7 +39,7 @@ public class Lab1 {
     cross.add(8,5);             // sensor up
     cross.add(6,7);             // sensor left
     cross.add(11,7);            // sensor right
-    cross.add(10,8);             // sensor down
+    cross.add(10,8);            // sensor down
     Critical up = new Critical(6);
     up.add(10,3);
     up.add(10,5);
@@ -50,8 +57,6 @@ public class Lab1 {
     t_1.start();
     Thread t_2 = new Thread(new Train(2,speed2));
     t_2.start();
-    
-
     try {
       tsi.setSpeed(1,speed1);
       tsi.setSpeed(2,speed2);
@@ -60,50 +65,33 @@ public class Lab1 {
       e.printStackTrace();    // or only e.getMessage() for the error
       System.exit(1);
     }
-
-
   }
 
-  private Critical isCritical(int x, int y){
+  public void setSwitch(int x, int y, int dir) {
+      try{
+        tsi.setSwitch(x,y, dir);
+      }
+
+      catch (CommandException e){
+        e.printStackTrace();
+        System.exit(1);
+
+      } 
+  }
+
+  public Critical isCritical(int x, int y){
     for (Critical c : sections) {
       for (Lab1.Critical.Sensor p : c.positions) {
         if(p.x == x && p.y == y){
           return c;
         }
       }
-      
     }
     return null;
   }
-
-  public void tryTrain(Critical c, Train t){
-    if(c.tryAcquire()) {
-      //if(sensor.isUp){switch.left or switch.right}
-
-      return;
-    }
-    else {
-      //find sensor
-      //if()
-      return;
-    }
-}
-
-public void setSwitch(int x, int y, int dir) {
-    try{
-      tsi.setSwitch(x,y, dir);
-    }
-
-    catch (CommandException e){
-      e.printStackTrace();
-      System.exit(1);
-
-    } 
-}
-
   
-  private class Critical extends Semaphore{
-      private ArrayList<Sensor> positions = new ArrayList(); 
+  public class Critical extends Semaphore{
+      private ArrayList<Sensor> positions = new ArrayList<Sensor>(); 
       private int id;
 
       private class Sensor {
@@ -138,16 +126,16 @@ public void setSwitch(int x, int y, int dir) {
     private final int id;
     private int speed;
     private SensorEvent sensor;
-    private Critical current;
-    private Critical rail;
     private boolean isAcquired = false;
-    //swtiches 15 9 right to zero cluster
-    // switches 17 7 last before 4-crossing
-    private boolean dir = true;
+    private boolean dir;
 
     public Train(int id, int speed){
       this.id = id;
       this.speed = speed;
+      if(id == 1)
+        this.dir = true;
+      else
+        this.dir = false;
     }
 
     void setSpeed(int speed){
@@ -169,22 +157,18 @@ public void setSwitch(int x, int y, int dir) {
       return -1;
     }
 
-    void check_track(Critical c, Train t){
+    void check_track(Critical c){
       int index = getIndex(c);
-      //TODO make exception
       
-      t.setSpeed(0);
+      this.setSpeed(0);
       switch(c.id) {
-        case(0): section_zero(t,index); break; 
-        case(2): section_two(t,index);  break;
-        case(4): section_four(t, index);break;
-        case(3): section_three(index); reverse_train(t);      break;
-        case(5): section_cross(t,index);break;
-        case(6): reverse_train(t);      break;
-
+      case(0): section_zero(index);                   break; 
+      case(2): section_two(index);                    break;
+      case(4): section_four(index);                   break;
+      case(3): section_three(index); reverse_train(c.id); break;
+      case(5): section_cross(index);                  break;
+      case(6): reverse_train(c.id);                       break;
       }
-
-      
     }
 
     public void changeSection(Critical c) {
@@ -196,34 +180,25 @@ public void setSwitch(int x, int y, int dir) {
         System.exit(1);
       }
     }
-
-    public void section_three(int sensor){
-      System.out.println("In here");
-      Critical section = sections.get(3);
-      if(sensor == 0){
-          System.out.println("Acquired");
-          section.tryAcquire();
-        }
-    }
   
-    public void section_zero (Train t, int sensor){
+    void section_zero (int sensor){
       int switch1_x = 3;
       int switch1_y = 11;
       int switch2_x = 4;
       int switch2_y = 9;
       Critical section = sections.get(0);
 
-      if(!t.isAcquired) {
+      if(!this.isAcquired) {
         changeSection(section);
         if(sensor == 2) {
           sections.get(3).release();
         }
         if(sensor == 0 || sensor == 2) {
           if (sections.get(1).tryAcquire()){
-            setSwitch(switch2_x, switch2_y, tsi.SWITCH_LEFT);
+            setSwitch(switch2_x, switch2_y, SWITCH_LEFT);
           }
           else {
-            setSwitch(switch2_x, switch2_y, tsi.SWITCH_RIGHT);
+            setSwitch(switch2_x, switch2_y, SWITCH_RIGHT);
           }
         }
         if(sensor == 1 || sensor == 3) {
@@ -231,54 +206,44 @@ public void setSwitch(int x, int y, int dir) {
             sections.get(1).release();
           }
           if(sections.get(3).tryAcquire()){
-            setSwitch(switch1_x, switch1_y, tsi.SWITCH_LEFT);
+            setSwitch(switch1_x, switch1_y, SWITCH_LEFT);
           }
           else{
-            setSwitch(switch1_x, switch1_y, tsi.SWITCH_RIGHT);
+            setSwitch(switch1_x, switch1_y, SWITCH_RIGHT);
           }
         }
-        t.isAcquired = true;
+        this.isAcquired = true;
       }
       else {
         section.release();
-        t.isAcquired = false;
+        this.isAcquired = false;
       }
       switch(sensor){
-        case(0): 
-        setSwitch(switch1_x, switch1_y, tsi.SWITCH_RIGHT);   
-        break;
-        case(1): setSwitch(switch2_x, switch2_y, tsi.SWITCH_LEFT);    break;
-        case(2): setSwitch(switch1_x, switch1_y, tsi.SWITCH_LEFT);    break;
-        case(3): setSwitch(switch2_x, switch2_y, tsi.SWITCH_RIGHT);   break;
+        case(0): setSwitch(switch1_x, switch1_y, SWITCH_RIGHT);   break;
+        case(1): setSwitch(switch2_x, switch2_y, SWITCH_LEFT);    break;
+        case(2): setSwitch(switch1_x, switch1_y, SWITCH_LEFT);    break;
+        case(3): setSwitch(switch2_x, switch2_y, SWITCH_RIGHT);   break;
       }
-      t.setSpeed(t.speed);
+      this.setSpeed(this.speed);
     }
 
-    public void section_two (Train t, int sensor){
-      System.out.println("Trying to catch");
+    void section_two (int sensor){
       int switch3_x = 15;
       int switch3_y = 9;
       int switch4_x = 17;
       int switch4_y = 7;
       Critical section = sections.get(2);
-      System.out.println(sensor);
-      if(sensor == 0){
-        System.out.println(t.isAcquired);
-      }
-      if(sensor == 1) {
-        System.out.println(t.isAcquired);
-      }
-      if(!t.isAcquired) {
+      if(!this.isAcquired) {
           changeSection(section);
           if(sensor == 0) {
             sections.get(1).release();
           }
           if(sensor == 0 || sensor == 2) {
             if(sections.get(4).tryAcquire()) {
-              setSwitch(switch4_x, switch4_y, tsi.SWITCH_RIGHT);
+              setSwitch(switch4_x, switch4_y, SWITCH_RIGHT);
             }
             else {
-              setSwitch(switch4_x, switch4_y, tsi.SWITCH_LEFT);
+              setSwitch(switch4_x, switch4_y, SWITCH_LEFT);
             }
           }
           if(sensor == 1) {
@@ -286,63 +251,76 @@ public void setSwitch(int x, int y, int dir) {
           }
           if(sensor == 1 || sensor == 3) {
             if(sections.get(1).tryAcquire()){
-              setSwitch(switch3_x, switch3_y, tsi.SWITCH_RIGHT);
+              setSwitch(switch3_x, switch3_y, SWITCH_RIGHT);
             }
             else {
-              setSwitch(switch3_x, switch3_y, tsi.SWITCH_LEFT);
+              setSwitch(switch3_x, switch3_y, SWITCH_LEFT);
             }
       
           }   
-          t.isAcquired = true;
+          this.isAcquired = true;
       }
     
       else {
         section.release();
-        t.isAcquired = false;
+        this.isAcquired = false;
         }
         
         
       switch(sensor){
-        case(0): setSwitch(switch3_x, switch3_y, tsi.SWITCH_RIGHT);  break;
-        case(1): setSwitch(switch4_x, switch4_y, tsi.SWITCH_RIGHT);  break;
-        case(2): setSwitch(switch3_x, switch3_y, tsi.SWITCH_LEFT);   break;
-        case(3): setSwitch(switch4_x, switch4_y, tsi.SWITCH_LEFT);   break;
+        case(0): setSwitch(switch3_x, switch3_y, SWITCH_RIGHT);  break;
+        case(1): setSwitch(switch4_x, switch4_y, SWITCH_RIGHT);  break;
+        case(2): setSwitch(switch3_x, switch3_y, SWITCH_LEFT);   break;
+        case(3): setSwitch(switch4_x, switch4_y, SWITCH_LEFT);   break;
       }
-      t.setSpeed(t.speed);
+      this.setSpeed(this.speed);
     }
 
-    void section_four(Train t, int sensor) {
+    void section_three(int sensor){
+      Critical section = sections.get(3);
+      if(sensor == 0){
+          section.tryAcquire();
+        }
+    }
+
+    void section_four(int sensor) {
       Critical section = sections.get(4);
       section.tryAcquire();
       this.setSpeed(this.speed);
     }
 
-    void section_cross(Train t, int sensor){
+    void section_cross(int sensor){
       Critical section = sections.get(5);
-       if(!t.isAcquired) {
+       if(!this.isAcquired) {
         changeSection(section);
-        t.isAcquired = true; 
+        this.isAcquired = true; 
        }
       else {
         section.release();
-        t.isAcquired = false;
+        this.isAcquired = false;
       }
       this.setSpeed(this.speed);
       
     }
 
-    void reverse_train(Train t){
+    void reverse_train(int id){
       try { 
-          System.out.print(t.dir);
-          if(this.dir){
+          if(this.dir && id == 3){
             this.dir = false;
+            this.setSpeed(0); 
+            Thread.sleep(2000+(20*this.speed));
+            this.speed = -this.speed;
             this.setSpeed(speed);
           }
-          else{
+          else if(!this.dir && id == 6) {
+            this.dir = true;
             this.setSpeed(0); 
-            Thread.sleep(2000+(20*t.speed));
+            Thread.sleep(2000+(20*this.speed));
             this.speed = -this.speed;
-            this.setSpeed(t.speed);
+            this.setSpeed(this.speed);
+          }
+          else {
+            this.setSpeed(speed);
           }
         }
         
@@ -350,18 +328,15 @@ public void setSwitch(int x, int y, int dir) {
           e.printStackTrace();
           System.exit(1);
         }
-      
-  
     }
+    
     public void run(){
       while(true){
         try {
           this.sensor = tsi.getSensor(this.id);
-          System.out.println(this.sensor.getXpos() + " " + this.sensor.getYpos());
           if(this.sensor.getStatus()==1) {
             Critical current = isCritical(sensor.getXpos(), sensor.getYpos());
-            System.out.println("sensor from " + current.id + " triggered");
-            check_track(current, this);
+            check_track(current);
           }
         } 
 
@@ -375,7 +350,6 @@ public void setSwitch(int x, int y, int dir) {
           e.printStackTrace();
           System.exit(1);
         }
-
 
         }
       }
